8a67fb0646dbe8b2e9dc839e0cc6c49ed776e25e
Solus90 <linley.phil@gmail.com>
2020-06-25 19:15:47 -0400

    first commit

---
+++ pages/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <h1>About DadJokes</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Magni voluptas ducimus vitae itaque! Tempore, similique expedita 
+          nesciunt, iste earum corporis repellendus porro minima iure, hic 
+          magni quae? Fugiat, eum perspiciatis.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: 'About The App',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
